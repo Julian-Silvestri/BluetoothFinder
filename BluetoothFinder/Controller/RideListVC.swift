7271247c200416c1346f7d324e088b55d00de812
Julian Silvestri <juliansilvestri92@gmail.com>
2023-02-05 22:16:57 -0500

    saving work

--- BluetoothFinder/Controller/RideListVC.swift
+++ BluetoothFinder/Controller/RideListVC.swift
@@ -7,14 +7,44 @@
 
 import UIKit
 
-class RideListVC: UIViewController {
+class CustomCollectionViewCell: UICollectionViewCell{
+    @IBOutlet weak var img: UIImageView!
+
+    
+}
+
+class RideListVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
 
     @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
     }
     
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 28
+    }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell",for:indexPath) as? CustomCollectionViewCell
+        cell?.img.image = UIImage(named: "icons8-kick-scooter-80.png")
+        cell?.layer.cornerRadius = 15
+        cell?.layer.borderWidth = 1
+        cell?.layer.borderColor = UIColor.white.cgColor
+        return cell ?? UICollectionViewCell()
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+      let height: CGFloat = 125.0
+        return CGSize(width: collectionView.bounds.width/2.5, height: height)
+    }
 
 
 }
--- BluetoothFinder/Controller/StoreVC.swift
+++ BluetoothFinder/Controller/StoreVC.swift
@@ -7,13 +7,65 @@
 
 import UIKit
 
-class StoreVC: UIViewController {
+class careAndProtectCollectionViewCell: UICollectionViewCell{
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var topLabel: UILabel!
+    @IBOutlet weak var middleLabel: UILabel!
+    @IBOutlet weak var bottomLabel: UILabel!
+    @IBOutlet weak var bottomView: UIView!
+}
 
+class accessoriesCollectioViewCell: UICollectionViewCell{
+    
+}
+
+class StoreVC: UIViewController,UICollectionViewDelegateFlowLayout, UICollectionViewDelegate, UICollectionViewDataSource{
+
+
+    @IBOutlet weak var profileBtn: UIButton!
+    @IBOutlet weak var careAndProtectSeeAllBtn: UIButton!
+    @IBOutlet weak var careAndProtectCollectionView: UICollectionView!
+    @IBOutlet weak var accessoriesCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        self.careAndProtectCollectionView.dataSource = self
+        self.careAndProtectCollectionView.delegate = self
+        self.profileBtn.layer.borderWidth = 1
+        self.profileBtn.layer.borderColor = UIColor.white.cgColor
+        self.profileBtn.layer.cornerRadius = 20
     }
 
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = careAndProtectCollectionView.dequeueReusableCell(withReuseIdentifier: "cellOne", for: indexPath) as? careAndProtectCollectionViewCell
+        
+        
+        if indexPath.row == 1{
+            cell?.topLabel.isHidden = true
+            cell?.middleLabel.text = "APOLLO"
+            cell?.bottomLabel.text = "CARE"
+        }
+//        cell?.bottomView.layer.cornerRadius = 10
+        cell?.bottomView.layer.borderWidth = 1
+        cell?.bottomView.layer.borderColor = UIColor.lightGray.cgColor
+        cell?.layer.cornerRadius = 10
+        cell?.layer.borderColor = UIColor.lightGray.cgColor
+        cell?.layer.borderWidth = 1
+        
+        return cell ?? UICollectionViewCell()
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,sizeForItemAt indexPath: IndexPath) -> CGSize {
+ 
+        return CGSize(width: 255, height: 328)
+    }
 
 }
